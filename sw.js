8a924e58a6f8d98e6bc1815beb44e7f9aa26bd08
henriquemattioli-alt <henriquemattioli@gmail.com>
2026-05-31 17:23:32 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,6 @@
-const CACHE = 'pao-duro-v1';
-const ASSETS = [
-  '/PAO-DURO/',
-  '/PAO-DURO/index.html',
-  '/PAO-DURO/manifest.json',
-  'https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { 
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
+self.addEventListener('fetch', e => {});
